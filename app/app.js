c6da665fd632694276ca73322e0ffed0c4b991c2
Jeff Cressman <jeff.cressman@gmail.com>
2014-09-27 11:30:13 +0930

    Added devTools and App to Global Scope for development

--- app/app.js
+++ app/app.js
@@ -2,17 +2,27 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import { setCsrfUrl } from 'rails-csrf';
+import ENV from 'ember-wknd/config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'ember-wknd', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+ 	ready: function() { // for devTools
+    this.devTools.globalize();
+  }
 });
 
 setCsrfUrl('http://localhost:3000/api/csrf');
 
 loadInitializers(App, 'rails-csrf');
 loadInitializers(App, 'ember-wknd');
+
+// TODO: only expose this in the development environment
+// Add App to Global scope so we can access it from the browser console
+if (ENV.environment === "development") {
+	window.App = App;
+}
 
 export default App;
