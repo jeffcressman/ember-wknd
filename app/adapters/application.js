f193c4690ba400610c422ee5fef1ddecda668dfd
Jeff Cressman <jeff.cressman@gmail.com>
2014-10-15 17:39:34 +1030

    Added TODO re using DS.ActiveModelAdapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,8 @@ import config from '../config/environment';
 // For testing without connecting to our Rails app
 // export default DS.FixtureAdapter.extend();
 
+// TODO: Why isn't this extending DS.ActiveModelAdapter?
+//       Probably would simplify code in other places.
 var ApplicationAdapter = DS.RESTAdapter.extend({
   
   host: config.APP.SERVER_URL
