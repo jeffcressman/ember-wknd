c1f2e187ba470b73a773c4b39239524c01851c9e
Jeff Cressman <jeff.cressman@gmail.com>
2014-10-15 16:23:37 +1030

    Now the rails-csrf issue is fixed.
    Last commit about rails-csrf removed a bunch of cruft but didn't fix the issue. The import statement changed slightly for the new version and I didn't see that.

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import { setCsrfUrl } from 'rails-csrf';
+import { setCsrfUrl } from 'rails-csrf/config';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
